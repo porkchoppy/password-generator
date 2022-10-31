f7932a0557506fc399501e635e30bde4e0770466
Christin Carter <raspberry.muppet@gmail.com>
2022-10-31 14:24:11 -0600

    Y/N while loops

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,4 @@
 // Assignment code here
-const numberCharacters = document.getElementById("numbers");
-const uppercaseCharacters = document.getElementById("uppercase");
-const lowercaseCharacters = document.getElementById("lowercase");
-const specialCharacters = document.getElementById("special");
-
 const numbers = "0123456789";
 const lowercase = "abcdefghijklmnopqrstuvwxyz";
 const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -15,17 +10,45 @@ var generateBtn = document.querySelector("#generate");
 //prompt user for pw criteria - pw length 8-128 chars - default minimum; lowercase, uppercase, numbers, spec chars
 
 var charLength = window.prompt("Select a length for your password between 8 and 128 characters.");
-var charNumbers = window.prompt("Would you like your password to contain numbers? Y/N")
-var charLower = window.prompt("Would you like your password to contain lowercase letters? Y/N")
-var charUpper = window.prompt("Would you like your password to contain uppercase letters? Y/N")
-var charSpecial = window.prompt ("Would you like your password to contain special characters? Y/N")
+
+var charNumbers = window.prompt("Would you like your password to contain numbers? Y/N");
+
+while(charNumbers.toUpperCase() !== "Y" && charNumbers.toUpperCase() !== "N"){
+  window.alert("Please enter Y for yes or N for no. ");
+  charNumbers = window.prompt("Would you like your password to contain numbers? Y/N");
+}
+
+var charLower = window.prompt("Would you like your password to contain lowercase letters? Y/N");
+
+while(charLower.toUpperCase() !== "Y" && charLower.toUpperCase() !== "N"){
+  window.alert("Please enter Y for yes or N for no. ");
+  charLower = window.prompt("Would you like your password to contain numbers? Y/N");
+}
+
+var charUpper = window.prompt("Would you like your password to contain uppercase letters? Y/N");
+
+while(charUpper.toUpperCase() !== "Y" && charUpper.toUpperCase() !== "N"){
+  window.alert("Please enter Y for yes or N for no. ");
+  charUpper = window.prompt("Would you like your password to contain numbers? Y/N");
+}
+
+var charSpecial = window.prompt ("Would you like your password to contain special characters? Y/N");
+
+while(charSpecial.toUpperCase() !== "Y" && charSpecial.toUpperCase() !== "N"){
+  window.alert("Please enter Y for yes or N for no. ");
+  charSpecial = window.prompt("Would you like your password to contain numbers? Y/N");
+}
+
+
+//var randomNumber = Math.floor(Math.random() * (128 - 8 + 1) ) + 8;  
+
 
 function generatePassword (){
 
 
-  //Math.floor(Math.random() * (128 - 8 + 1) ) + 8;  
- 
-  //validate input
+
+
+//validate input
 
   //generate pw based on criteria
 
