77e40b0f13eb1203d2276f2892f2957f662d88c0
Christin Carter <raspberry.muppet@gmail.com>
2022-10-30 23:43:31 -0600

    defined variables for prompts

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,19 +4,26 @@ const uppercaseCharacters = document.getElementById("uppercase");
 const lowercaseCharacters = document.getElementById("lowercase");
 const specialCharacters = document.getElementById("special");
 
+const numbers = "0123456789";
+const lowercase = "abcdefghijklmnopqrstuvwxyz";
+const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const special = "!@#$%^&*+_?"
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+//prompt user for pw criteria - pw length 8-128 chars - default minimum; lowercase, uppercase, numbers, spec chars
+
+var charLength = window.prompt("Select a length for your password between 8 and 128 characters.");
+var charNumbers = window.prompt("Would you like your password to contain numbers? Y/N")
+var charLower = window.prompt("Would you like your password to contain lowercase letters? Y/N")
+var charUpper = window.prompt("Would you like your password to contain uppercase letters? Y/N")
+var charSpecial = window.prompt ("Would you like your password to contain special characters? Y/N")
+
 function generatePassword (){
 
-    //prompt user for pw criteria - pw length 8-128 chars - default minimum; lowercase, uppercase, numbers, spec chars
-  var userSelection = window.prompt("Select a length for your password from 8 to 128 characters.", 8);
-
-
-  //prompt user for pw criteria
-    //pw length 8-128 chars - default minimum 
-    //lowercase, uppercase, numbers, spec chars
   
+ 
   //validate input
 
   //generate pw based on criteria
